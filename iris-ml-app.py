6f7bafec371a2ff7384308d795e721779f6ed4fc
rDhiaz <74031371+rDhiaz@users.noreply.github.com>
2022-08-24 13:20:45 +0800

    Update iris-ml-app.py

--- iris-ml-app.py
+++ iris-ml-app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-
+from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
@@ -28,8 +28,8 @@ df = user_input_features()
 
 st.subheader('User Input parameters')
 st.write(df)
-iris = pd.read_csv('https://raw.githubusercontent.com/rDhiaz/streamlit-example/main/IRIS.csv')
-#iris = datasets.load_iris()
+#iris = pd.read_csv('https://raw.githubusercontent.com/rDhiaz/streamlit-example/main/IRIS.csv')
+iris = datasets.load_iris()
 X = iris.data
 Y = iris.target
 
